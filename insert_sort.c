485772067c4db64022d444ba2c1851d264beea85
MaSander <msander@casca.localdomain>
2023-01-28 20:43:39 -0300

    delete lst_print's and trash's

--- insert_sort.c
+++ insert_sort.c
@@ -1,16 +1,5 @@
 #include "push_swap.h"
 
-// static void put_smaller_on_top(t_stack *s, char stack)
-// {
-// 	int lower_index;
-
-// 	lower_index = find_smallest_index(s);
-// 	while (s->index != lower_index)
-// 	{
-// 		s = ft_rotate(s, stack);
-// 	}
-// }
-
 void insert_sort(t_ps *ps)
 {
 	int lower_index;
@@ -22,10 +11,8 @@ void insert_sort(t_ps *ps)
 		{
 			ps->stack_a = ft_rotate(ps->stack_a, 'a');
 		}
-		ft_lstprint(ps->stack_b);
 		ft_push(&ps->stack_a, &ps->stack_b, 'b');
 	}
-	ft_lstprint(ps->stack_b);
 	while (ps->stack_b)
 	{
 		ft_push(&ps->stack_b, &ps->stack_a, 'a');
