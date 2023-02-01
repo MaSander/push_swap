bc16f8a8b3ca79db6ac2aee713223b11ea0eed66
Otavio_Burato <otavioburato42@gmail.com>
2023-01-31 23:02:13 -0300

    fix

--- push_swap.c
+++ push_swap.c
@@ -38,15 +38,14 @@ int	main(int argc, char **argv)
 
 	if (init(&ps, argc, argv) != FALSE)
 	{
-		ps.stack_a = ft_reverse_rotate(ps.stack_a, 'a');
-		// index_queue(ps.stack_a, ps.lst_len);
+		index_queue(ps.stack_a, ps.lst_len);
 
-		// if(ps.lst_len <= 3)
-		// 	ps.stack_a = sort_three(ps.stack_a);
-		// else if(ps.lst_len <= 5)
-		// 	sort_five(&ps);
-		// else
-		// 	ft_radix(&ps);
+		if(ps.lst_len <= 3)
+			ps.stack_a = sort_three(ps.stack_a);
+		else if(ps.lst_len <= 5)
+			sort_five(&ps);
+		else
+			ft_radix(&ps);
 		ft_lstprint(ps.stack_a);
 		ft_lstclear(&ps.stack_a);
 		ft_lstclear(&ps.stack_b);
